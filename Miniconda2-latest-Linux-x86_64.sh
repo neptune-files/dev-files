ac57ef41a30e27b9eac7955654f1f988e837c9ce
Lalitha Oruganty <lorugant@device.localdomain>
2019-06-12 15:59:46 -0400

    bash scripts in dev build file

---
+++ Miniconda2-latest-Linux-x86_64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3e20425afa1a2a4c45ee30bd168b90ca30a3fdf8598b61cb68432886aadc6f4d
+size 45117115
---
+++ bazel-0.5.3-installer-linux-x86_64.sh
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ca25e9191713122592ef1fa188b903beea50dc8a18d336e983b7310196fddd6b
+size 211136091
